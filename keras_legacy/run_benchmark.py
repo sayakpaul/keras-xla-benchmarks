2024ae906ff7a9ece1906630161f17f24c18aa00
Sayak Paul <spsayakpaul@gmail.com>
2023-05-28 14:34:22 +0530

    add: benchmarking code.

---
+++ keras_legacy/run_benchmark.py
@@ -0,0 +1,102 @@
+import argparse
+import tensorflow as tf
+from model_mapping import MODEL_NAME_MAPPING
+import time
+import wandb
+from utilities import get_flops
+
+BATCH_SIZE = 4
+WARMUP_ITERATIONS = 10
+NUM_ITERATIONS = 100
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_family",
+        type=str,
+        default="ResNet_V1",
+        required=True,
+        choices=list(MODEL_NAME_MAPPING.keys()),
+        help="Model family to benchmark.",
+    )
+    parser.add_argument(
+        "--resolution",
+        type=int,
+        default=None,
+        help="Resolution to use for benchmarking when model input spec is not fully defined.",
+    )
+    parser.add_argument(
+        "--xla", action="store_true", help="XLA-compile the model variants."
+    )
+    parser.add_argument("--log_wandb", action="store_true", help="Log to WandB.")
+    args = parser.parse_args()
+    return args
+
+
+def main(args):
+    print(f"Running benchmark for {args.model_family}...")
+    all_model_variants = MODEL_NAME_MAPPING.get(args.model_family)
+    print(f"Total model variants: {len(all_model_variants)}.")
+
+    for variant in all_model_variants:
+        # Retrieve the current model variant.
+        model = all_model_variants[variant]
+        assert isinstance(model, tf.keras.Model)
+
+        # Determine the input spec with which to run the benchmark.
+        input_spec_shape = [BATCH_SIZE] + model.inputs[0].shape[1:]
+        if args.resolution is None:
+            args.resolution = input_spec_shape[1]
+        if input_spec_shape[1] is None and args.resolution is None:
+            raise ValueError(
+                "When model input spec is not fully defined, one must specify a valid `resolution`."
+            )
+
+        # XLA compilation.
+        print(f"Compiling with XLA: {args.xla}...")
+        if args.xla:
+            model = tf.function(model, jit_compile=True)
+
+        # Generate a batch of random inputs and warm the model up.
+        print("Warming up the model...")
+        random_inputs = tf.random.normal(input_spec_shape)
+        for _ in range(WARMUP_ITERATIONS):
+            _ = model(random_inputs, training=False)
+
+        # Calculate throughput.
+        print("Calculating throughput...")
+        start_time = time.time()
+        for _ in range(NUM_ITERATIONS):
+            _ = model(random_inputs, training=False)
+        end_time = time.time()
+        total_time = end_time - start_time
+        throughput = NUM_ITERATIONS * BATCH_SIZE / total_time
+        print("Throughput: {:.2f} samples per second".format(throughput))
+
+        # Calculate FLOPs and number of parameters.
+        num_params = model.count_params() / 1e6
+        flops = (get_flops(model, input_spec_shape) / 1e9) / BATCH_SIZE
+        print(f"Model parameters (million): {num_params:.2f}")
+        print(f"FLOPs (giga): {flops:.2f}")
+
+        # Log to WandB if specified.
+        if args.log_wandb:
+            run_name = f"{variant}@xla-{args.xla}@res-{args.args.resolution}"
+            wandb.init(project="keras-xla-benchmarks", nam=run_name, config=args)
+            wandb.log(
+                {
+                    "Model family": args.model_family,
+                    "Model variant": variant,
+                    "XLA": args.xla,
+                    "Throughput (samples/sec)": f"{throughput:.2f}",
+                    "Num parameters (million)": f"{num_params:.2f}",
+                    "FLOPs (giga)": f"{flops:.2f}",
+                }
+            )
+            wandb.finish()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
---
+++ utilities/calculate_flops.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+from tensorflow.python.framework.convert_to_constants import (
+    convert_variables_to_constants_v2_as_graph,
+)
+
+
+def get_flops(model, input_shape=(4, 224, 224, 3)):
+    """
+    Calculate FLOPS for a `tf.keras.Model`.
+    Ignore operations used in only training mode such as Initialization.
+    Use tf.profiler of tensorflow v1 api.
+
+    Args:
+        regnety_instance: A regnety.models.model.RegNetY instance
+
+    Returns:
+        Tuple containing total float ops and paramenters
+
+    Adapted from:
+    https://github.com/AdityaKane2001/regnety/blob/1c0e3e6978e97bd8bc1c6eb3b100c012c2fc702a/regnety/utils/model_utils.py#L8
+    """
+    inputs = [tf.TensorSpec(input_shape, tf.float32)]
+    real_model = tf.function(model).get_concrete_function(inputs)
+    frozen_func, _ = convert_variables_to_constants_v2_as_graph(real_model)
+    run_meta = tf.compat.v1.RunMetadata()
+    opts = tf.compat.v1.profiler.ProfileOptionBuilder.float_operation()
+    flops = tf.compat.v1.profiler.profile(
+        graph=frozen_func.graph, run_meta=run_meta, cmd="scope", options=opts
+    )
+    return flops.total_float_ops // 2, flops.parameters
