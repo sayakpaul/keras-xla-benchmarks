dc028433c7e99f40111e99e0479518216f42a8b9
Sayak Paul <spsayakpaul@gmail.com>
2023-05-28 15:11:30 +0530

    fix: wandb init args

--- keras_legacy/run_benchmark.py
+++ keras_legacy/run_benchmark.py
@@ -93,7 +93,7 @@ def main(args):
         # Log to WandB if specified.
         if args.log_wandb:
             run_name = f"{variant}@xla-{args.xla}@res-{args.resolution}"
-            wandb.init(project="keras-xla-benchmarks", nam=run_name, config=args)
+            wandb.init(project="keras-xla-benchmarks", name=run_name, config=args)
             wandb.log(
                 {
                     "Model family": args.model_family,
