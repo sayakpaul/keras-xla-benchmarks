6109246937f466c8187a6258acacf86937d91fd0
Sayak Paul <spsayakpaul@gmail.com>
2023-05-31 19:34:12 +0530

    fix: variant benchmarking.

--- keras_legacy/run_all_benchmarks.py
+++ keras_legacy/run_all_benchmarks.py
@@ -4,14 +4,11 @@ import subprocess
 
 def main():
     for model_family in MODEL_NAME_MAPPING:
-        run_command_no_xla = (
-            f"python run_benchmark.py --model_family {model_family} --log_wandb"
-        )
-        run_command_xla = (
-            f"python run_benchmark.py --model_family {model_family} --xla --log_wandb"
-        )
-        for command in [run_command_no_xla, run_command_xla]:
-            _ = subprocess.run(command.split())
+        for variant in model_family:
+            run_command_no_xla = f"python run_benchmark.py --model_family {model_family} --model_variant {variant} --log_wandb"
+            run_command_xla = f"python run_benchmark.py --model_family {model_family} --model_variant {variant} --xla --log_wandb"
+            for command in [run_command_no_xla, run_command_xla]:
+                _ = subprocess.run(command.split())
 
 
 if __name__ == "__main__":
