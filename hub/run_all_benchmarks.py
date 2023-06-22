704fda4408267242ab84b0fc54ebd0f36b57855e
Sayak Paul <spsayakpaul@gmail.com>
2023-06-22 19:39:46 +0530

    fix benchmark bulk.

--- hub/run_all_benchmarks.py
+++ hub/run_all_benchmarks.py
@@ -6,11 +6,10 @@ from model_mapping import MODEL_NAME_MAPPING
 def main():
     for model_family in MODEL_NAME_MAPPING:
         for variant in MODEL_NAME_MAPPING[model_family]:
-            for res in [320, 640]:
-                run_command_no_xla = f"python run_benchmark.py --model_family {model_family} --model_variant {variant} --resolution {res} --log_wandb"
-                run_command_xla = f"python run_benchmark.py --model_family {model_family} --model_variant {variant} --resolution {res} --xla --log_wandb"
-                for command in [run_command_no_xla, run_command_xla]:
-                    _ = subprocess.run(command.split())
+            run_command_no_xla = f"python run_benchmark.py --model_family {model_family} --model_variant {variant} --log_wandb"
+            run_command_xla = f"python run_benchmark.py --model_family {model_family} --model_variant {variant} --xla --log_wandb"
+            for command in [run_command_no_xla, run_command_xla]:
+                _ = subprocess.run(command.split())
 
 
 if __name__ == "__main__":
